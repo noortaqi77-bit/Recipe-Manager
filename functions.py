13378a165327c16069093a1f51f831d4246183f6
Noor Taqi <noortaqi77@gmail.com>
2026-09-22 14:00:57 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -58,7 +58,7 @@ def last_cooked(recipe,date, df):
 #find oldest dates
 def oldest_recipe(df):
     oldest=df.sort_values(by ="Last Date")
-    return oldest.head(2)
+    return oldest.head(1)
 
 # Assistant:
 def get_llm(prompt):
