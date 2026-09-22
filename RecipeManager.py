87475cfdfde6a11e773ac458b43b4c74ae4fa7a6
Noor Taqi <noortaqi77@gmail.com>
2026-09-22 13:34:45 +0300

    Add files via upload
    p

---
+++ RecipeManager.py
@@ -0,0 +1,162 @@
+import streamlit as st
+import pandas as pd 
+import functions
+import datetime
+import time as t
+
+
+data=functions.load_data('recipes.csv')
+st.set_page_config(layout="wide")
+st.title("Delicious Recipes 🍴")
+
+
+tab0,tab1, tab2, tab3, tab4, tab5, tab6 , tab7, tab8= st.tabs(["Home", "Add New Recipes", "Find Recipes", "All Recipes", "Random Recipes", "Recipe's Rate", "Shopping List" , "Cooking History", "Smart Chef"])
+
+with tab0:
+    st.text("Welcome !...")
+    st.image("https://static.vecteezy.com/system/resources/previews/022/727/413/original/cookbook-of-recipes-baking-ingredients-products-and-kitchen-tools-for-cooking-cartoon-illustration-vector.jpg", width=450)
+
+#Add a new recipe to the collection  
+with tab1:
+    st.subheader("Add New Recipe")
+    recipe=st.text_input("Recipe name 🍽️:")
+    ingredients=st.text_area("The Ingredients 🫕:")
+    ingredient1=ingredients.replace(",", "\n")
+    ingredient=ingredient1.split("\n")
+    time=st.slider("Preparation time 'in minutes' ⏱️", min_value=1, max_value=300, value=10)
+    step=st.text_area("Cooking instructions 🔢:")
+    level_=st.radio("Level 🎚️", ["Easy", "Medium", "Hard"])
+    category= st.selectbox("category 🍴",["breakfast", "lunch", "dinner","desert"], index=None,placeholder="Select category",
+    accept_new_options=True)
+    sentiment_mapping = [1, 2, 3, 4, 5]
+    st.markdown("Rating 👍👎")
+    rate = st.feedback("stars")
+    if rate is not None:
+         st.markdown(f"You selected {sentiment_mapping[rate]} star(s).")
+    date=st.date_input("Last cooked date: 📅")
+    Date=str(date)
+
+
+
+    buttom=st.button("Save The Recipe")
+    
+    if buttom:
+        if recipe and ingredient and time and step and level_ and category and rate:
+            
+            row={"Recipe":recipe,
+                        "Ingredients":", ".join(ingredient),
+                        "Time (in minutes)":time,
+                         "instructions":step,
+                         "Level":level_,
+                         "Category":category, 
+                         "Rate":rate, 
+                         "Last Date":Date
+                        }
+            
+            row_df=pd.DataFrame([row])
+            # new=pd.concat(row_df, rn)
+            row_df.to_csv("recipes.csv", mode="a", header=False, index=False)
+            st.success("Recipe saved successfully")
+            t.sleep(5)
+            st.rerun()
+        else:
+            st.error("Please fill required fields!!")
+            
+            
+
+#1. The program asks for an ingredient to search for  
+#2. The program displays all recipes that contain that ingredient 
+with tab2:
+    st.header("Find Recipes by ingredients 🔎!")
+    find=st.text_input("Enter an ingradient : ")
+    button2=st.button("Search..")
+    search=functions.search_recipe(find, data)
+
+    if button2:
+        if search.empty:
+            st.error("There is no recipe with this ingradient!")
+        else:
+            st.write(search)
+    st.divider()
+    st.subheader("search from another data")
+    dash=st.text_input("Enter the dash ")
+    if st.button("search"):
+        if dash:
+            lis=functions.search_api(dash)
+            if lis is None:
+                st.text("No recipes found with this name")
+            else:
+                new_list=st.dataframe(lis)
+                # new_list2=new_list.T
+                # if st.button("save"):
+                #     new_list.to_csv("recipes.csv", mode="a", header=False, index=False)
+                #     st.success("Recipe saved successfully")
+                #     t.sleep(5)
+                #     st.rerun()            
+
+# The program displays a list of all recipe names with their preparation times         
+with tab3:
+    st.header("All Recipes 🍽️")
+    st.table(data[["Recipe","Time (in minutes)"]].reset_index(drop=True))
+    #st.dataframe(r, hide_index=True, use_container_width=True)
+    
+#random recipes
+with tab4:
+    st.header("Random Recipes ")
+    st.dataframe(functions.show_random(data))
+
+# The program allows users to rate recipes and sort by rating  
+with tab5:
+    st.header("Recpies by Rating ⭐ ")
+    st.dataframe(data.sort_values("Rate", ascending=False).reset_index(drop=True))
+
+#4. The program can generate a shopping list based on selected recipes  
+with tab6:
+    st.header("Shopping list 🛒 ")
+    lists=st.multiselect("choose recipe", options=data["Recipe"])
+    if lists:
+        shop=functions.shopping_list(lists, data)
+        st.text("Your Ingredients List 📋")
+        for i in shop:
+            st.checkbox(i)
+
+#The program can track cooking history and suggest recipes you haven't made recently 
+with tab7:
+    st.header(" Track Cooking History ")
+    st.text("Update Date for a Recipe")
+    selected_recipe=st.selectbox("select recipe", data["Recipe"].unique())
+    new_date=st.date_input("new date")
+    if st.button("Update.."):
+        functions.last_cooked(selected_recipe, new_date, data)
+        st.success(f"Updated {selected_recipe} date successfully! ")
+        t.sleep(5)
+        st.rerun()
+    
+    st.divider()
+    
+    st.subheader("Oldest recipes..")
+    st.write(functions.oldest_recipe(data))
+
+    
+#Ai help
+with tab8:
+    st.subheader("How can I help you 🤖")
+    respone=st.text_input("Enter your Ingrediends")
+    
+    click=st.button("Ask AI")
+    if click:
+        if respone:
+            result=functions.get_llm(respone)
+            st.write(result)
+        else:
+            st.warning("please enter your ingrediens")
+
+# with tab9:
+#     select_recipe=st.selectbox("choose recipe to scale :", data["Recipe"])
+#     serving=st.number_input("How many serving do ypu want ? ")
+#     scale=st.button("Scale Serving")
+#     if scale :
+#         st.write(functions.scale_recipe(select_recipe,serving, data))
+        
+
+
---
+++ functions.py
@@ -0,0 +1,124 @@
+import pandas as pd
+import datetime 
+from openai import OpenAI
+import requests
+import os
+from dotenv import load_dotenv
+
+# 1. Initialize the client by api key: 
+load_dotenv('key')
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key= os.getenv('key')
+
+)
+
+#data file
+def load_data(path):
+    df=pd.read_csv(path, names=["Recipe", "Ingredients", "Time (in minutes)", "instructions", "Level", "Category", "Rate", "Last Date"], header=0)
+    return df
+
+# 5-random recipes:
+def show_random(df):
+    return df.sample()
+
+  
+# def search_recipes(find, df):
+#     index_list = []
+#     for index, row in df.iterrows():
+#         ingredients_list=row['Ingredients'].split(",")
+#         for ing in row['Ingredients']:
+#            if ing.lower() == find.lower():
+#               index_list.append(index)
+#     return df.iloc[index_list,:]  
+
+# 3 seach by ingredients
+def search_recipe(find, df):
+    search=df[df["Ingredients"].str.contains(find, case=False)]
+    return search
+
+# shopping list step
+def shopping_list(selected, df):
+    shop=[]
+    filter_recipe=df[df["Recipe"].isin(selected)]
+    for i in filter_recipe["Ingredients"]:
+        #split the row it self
+        item=str(i).split(",")
+        for j in item:
+            if j:
+                shop.append(j)
+    return shop
+
+#track recipes history
+def last_cooked(recipe,date, df):
+    data_new=str(date)
+    df.loc[df["Recipe"]==recipe , "Last Date"]=data_new
+    df.to_csv("recipes.csv", index=False)
+
+#find oldest dates
+def oldest_recipe(df):
+    oldest=df.sort_values(by ="Last Date")
+    return oldest.head(1)
+
+# Assistant:
+def get_llm(prompt):
+    completion = client.chat.completions.create(
+        model="cohere/north-mini-code:free",
+        
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a chef assistant . provide a recipes ideas by the ingredients",
+            },
+            {"role": "user", "content": prompt},
+        ],
+        temperature=0.0,
+    )
+    response = completion.choices[0].message.content
+    return response
+
+def search_api(dash):
+    url="https://themealdb.com/api/json/v1/1/search.php"
+    response = requests.get(url,
+                    params={"s": dash, 'format':'json'}) 
+    api=response.json()
+    if api["meals"] is None:
+        return None
+    meal=api["meals"][0]
+    list_ingre=[]
+#random number 25
+    for i in range(1,25):
+         item=meal.get(f"strIngredient{i}")
+         if item and item != "":
+             list_ingre.append(item)
+             dateframe_api={
+                "Recipe":meal["strMeal"],
+                        "Ingredients":", ".join(list_ingre),
+                        "Time (in minutes)":30,
+                         "instructions":meal["strInstructions"],
+                         "Level":"medium",
+                         "Category":meal.get("strCategory", ""),
+                         "Rate":3, 
+                         "Last Date":datetime.date.today()
+            }
+    return dateframe_api
+
+ 
+# def scale_recipe(select_recipe, serving, df):
+#     ingredient_list=[]
+#     filter_recipe=df[df["Recipe"]==select_recipe]
+#     split_item=filter_recipe["Ingredients"].values[0]
+
+#     for item in split_item:
+#         splits=item.split()
+#         num=float(splits[1])
+#         new_intrgre=" ".join(splits[1:])
+#         new_scale=num*serving
+#         ingredient_list.append(f"{new_scale} {new_intrgre}")
+#     return ", ".join(ingredient_list)
+       
+    
+        
+
+
+    
