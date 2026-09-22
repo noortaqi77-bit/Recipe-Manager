9cd9dde3471424be488e4afd119b87b8ecfd8889
Noor Taqi <noortaqi77@gmail.com>
2026-09-22 19:23:34 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -123,7 +123,7 @@ def serving_recipe(items, serving):
     scale=[]
     for item in items:
         shop=item.split()
-        new=float(shop[0])*serving
+        new=shop[0]*serving
         rest=shop[1]
         scale.append(f"{new} {rest}")
     return scale
